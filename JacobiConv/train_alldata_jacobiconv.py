58e37fd3854dfaf3700e33ec8956d997f9249608
jzhu1905 <jiaqi.zhu@mail.mcgill.ca>
2024-11-17 17:00:23 -0500

    Update main

--- JacobiConv/train_alldata_jacobiconv.py
+++ JacobiConv/train_alldata_jacobiconv.py
@@ -223,3 +223,5 @@ def train_alldata_jacobiconv(device: torch.device,
 if __name__ == "__main__":
     args = utils.parse_args()
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+    train_alldata_jacobiconv(device, args)
